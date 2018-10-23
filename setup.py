2d8365e20b8a0931022b829758a143684112dd47
ArndalAndersen <camilla@arndalandersen.dk>
2018-10-23 10:45:22 +0200

    PKG: Try to include frontend in package distribution

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import glob
 
 try:
     from setuptools import setup  #, find_packages
@@ -8,5 +9,11 @@ except ImportError:
     raise sys.exit('Could not import setuptools.')
 
 
-setup(packages=['pyqmix_web'],
-      package_dir={'pyqmix_web': 'pyqmix_backend'})
+files = glob.glob('pyqmix_frontend/build/*')
+
+setup(
+    packages=['pyqmix_web', 'pyqmix_web.fronted'],
+    package_dir={'pyqmix_web': 'pyqmix_backend',
+                 'pyqmix_web.fronted': 'pyqmix_frontend'},
+    include_package_data=True  # Include files from MANIFEST.in
+)
