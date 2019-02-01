40ed993c9ceeb45803a231231f5d8859ee33a88b
Richard Höchenberger <richard.hoechenberger@gmail.com>
2019-02-01 14:57:18 +0100

    PKG: Check if frontend production build exists before proceeding

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 import sys
+import pathlib
 import versioneer
 
 try:
     from setuptools import setup
 except ImportError:
     raise sys.exit('Could not import setuptools.')
+
+if not pathlib.Path('./pyqmix_frontend/build').exists():
+    msg = 'Please run `npm build` in the `pyqmix_frontend` directort first.'
+    raise RuntimeError(msg)
 
 setup(
     version=versioneer.get_version(),
